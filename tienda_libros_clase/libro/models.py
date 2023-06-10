5b095158a55e713d50f4c77d31a27705bb28d514
Vera-Ve <vera.savina.ve@gmail.com>
2023-06-10 15:19:57 +0300

    Eliminar carpeta <nombre-de-la-carpeta>

--- tienda_libros_clase/libro/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Libro(models.Model):
-    titulo = models.CharField(max_length=50)
-    author = models.CharField(max_length=100)
--- tienda_libros_clase/libro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.api_libros),
-    path('<int:pk>', views.LibroRetrieveAPIView.as_view()),
-    path('create', views.LibroListCreateAPIView.as_view()),
-]
